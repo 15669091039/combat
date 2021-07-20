e4b08be1fcbfb26e6aab5a289f13c01ed6888cd5
15669091039 <137934119@qq.com>
2021-07-20 14:18:48 +0800

    测试生成器，得出结论 使用yield只能减少内存使用， 时间上不占优势

--- Application/chap03/chap_03_01_function.php
+++ Application/chap03/chap_03_01_function.php
@@ -5,3 +5,40 @@
  * Time: 10:05
  * When you read this code, good luck for you.
  */
+declare(strict_types=1);
+function getParams(...$args){
+    return var_export($args,true);
+}
+
+function someDirScan($dir){
+    static $list=[];
+    $list=glob($dir.DIRECTORY_SEPARATOR.'*');
+    foreach ($list as $item){
+        if (is_dir($item)){
+            $list=array_merge($list,someDirScan($item));
+        }
+    }
+    return $list;
+}
+
+function someTypeHint(array $a,int $b,string $c):int{
+    try {
+        return 'ssss';
+        //someTypeHint([],66,88);
+    }catch (Throwable $e){
+        echo  $e->getMessage();
+        echo $e->getFile();
+        echo $e->getLine();
+        echo $e->getCode();
+        die;
+    } finally {
+
+    }
+
+}
+echo  someTypeHint([],32,'55');
+
+
+
+
+
